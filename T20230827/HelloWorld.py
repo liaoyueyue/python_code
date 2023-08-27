751c7a68c9a1be13e57b3dd9b7833058b8fce8f5
liaoyueyue <973044869@qq.com>
2023-08-27 18:06:09 +0800

    study: 20230827

---
+++ T20230827/HelloWorld.py
@@ -0,0 +1,25 @@
+# 1
+def print_hello_world():
+    print(f"This is my first python program")
+
+
+print_hello_world()
+
+# 2
+name = 'alice'
+print(type(name))
+
+age = 18
+print(type(age))
+
+print(f"我的名字是{name}, 今年{age}")
+
+# 3
+print("我的姓名是", end=":")
+print("alice")
+
+# 4
+x = "x"
+y = "y"
+print(x, y, "z")
+print(x, y, 'z', sep=", ")
